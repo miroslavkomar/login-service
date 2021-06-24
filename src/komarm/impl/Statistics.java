60b78f576b50fa9d7de38ef31c5fe03f1e688c21
miroslavkomar <49066672+miroslavkomar@users.noreply.github.com>
2021-06-24 10:36:50 +0200

    Update Statistics.java
    statistics submit two

--- src/komarm/impl/Statistics.java
+++ src/komarm/impl/Statistics.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Statistics {
 
-    public static ArrayList<JSONObject> requestRecords(komarm.impl.LoginLogsAggService loginLogsAggService){
+    public static ArrayList<JSONObject> requestRecords(LoginLogsAggService loginLogsAggService){
         Scanner input = new Scanner(System.in);
         System.out.println("Enter number of top countries");
         int num = input.nextInt();
